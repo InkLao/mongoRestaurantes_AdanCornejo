dbf755ae34a95cb3d7b46c5c39a231f0730c6dc7
InkLao <eduardo.cornejo.balcazar@gmail.com>
2024-04-30 12:26:03 -0700

    Creación del repositorio

---
+++ src/main/java/com/mycompany/mongorestaurantes_228558/MongoRestaurantes_228558.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mongorestaurantes_228558;
+
+/**
+ *
+ * @author eduar
+ */
+public class MongoRestaurantes_228558 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
